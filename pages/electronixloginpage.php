ae036c4add290b3c4c5baacebfd6f70de6861d62
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-10 02:57:31 +0000

    Refactor login page & make changes to stylesheet

--- pages/electronixloginpage.php
+++ pages/electronixloginpage.php
@@ -1,82 +1,56 @@
-<?php include('includes\server.php') ?>
+<?php include('../includes/server.php') ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>HOME</title>
-        <link href="style.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="style.css" rel="stylesheet" type="text/css">
-        <script src="flex.js"></script>
-
-        <style>
-        body {margin:0;}
-        .icon-bar {width: auto;background-color: rgb(117, 116, 116);overflow: auto;}
-        .icon-bar a {float: left;width: 20%;text-align: center;padding: 5px 0;transition: all 0.3s ease;color: white;font-size: 25px;}
-        .icon-bar a:hover {background-color: #000;}
-        .active {background-color: green;}
-
-        p.success,
-        p.error {
-            color: white;
-            font-family: lato;
-            background: yellowgreen;
-            display: inline-block;
-            padding: 2px 10px;
-        }
-        p.error {
-            background: orangered;
-        }
-        </style>
+        <link href="../assets/css/style.css" rel="stylesheet" type="text/css">
     </head>
 
-    <body style="background-color: #E0FFFF">
-
-            <div style="color:white"></div>
+    <body>
+        <div class="header">
             <center>
-            <img src="logoo.png" style="width: 50px; height: 50px;float: left">
-            <h1 style="color: blue; background-color: white;font-family: 'Times New Roman', Times, serif;font-size: xxx-large;">Electronix, Inc.</h1>
-            <p style="color: green">World-class engineering solutions @YOUR FINGERTIPS</p>
-            </center>
+            <img src="../assets/images/logo2.png" class="logo">
+            <h1 class="heading">Electronix Inc.</h1>
+            <p class="sub-heading">World-class engineering solutions @YOUR FINGERTIPS</p>
+            </center>  
+        </div>    
 
-            <div class="icon-bar">
-                <a class="active" href="./electronixloginpage.php">Home</a> 
-                <!--<a href="./electronixselect.php"><i class="fa fa-bookmark"></i></a>-->
-                <a href="./electronixregisterpage.php">Register</i></a> 
-                <a href="./Contact Us.html"><i class="fa fa-address-card"></i></a>
-            </div>
+        <div class="icon-bar">
+            <a class="active" href="./electronixloginpage.php">Home</a> 
+            <a href="./electronixregisterpage.php">Register</i></a> 
+            <a href="./Contact Us.html"><i class="fa fa-address-card"></i></a>
+        </div>
 
-            <div class="div1">
-            </div>
+        <div class="div1">
+        </div>
             
-            <div>
+        <div>
             <center class="login" >
                 <h1>Please enter your login details</h1>
                     <form method="POST" action="electronixloginpage.php">
-                        <?php include('errors.php'); ?>
+                        <?php include('../includes/errors.php'); ?>
                         
 				        <input type="text" name="email" placeholder="Email" >
                         <br><br>
                         
                         <input type="password" name="password" placeholder="Password">
                         <br><br>
-                    
-                      
                         
                         <input type="submit" name="login_user">
                         <p>Don't have an account? <a href="electronixregisterpage.php">Sign up now</a>.</p>
                     </form>  
             </center>
-            </div>
+        </div>
             
             
-            <div style="background-color: white;height: 500 px;">...</div>
-            <footer>
-                <div style="background-color: #2868c7;height: 15 px;">
-                <marquee><p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;font-size: medium;color: white;">ELECTRONIX 2021</p></marquee>
 
+        <footer>
+            <div style="background-color: white;height: 15 px;">
+                <marquee><p style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;font-size: medium;color: #2868c7;">ELECTRONIX 2021</p></marquee>
             </div>
-            </footer> 
+        </footer> 
         
     </body>
 </html>
