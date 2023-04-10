480fe700fba7331a81b72ec8e04fdab4a2fb52b8
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-04-10 00:57:55 +0000

    added gitignore for config file

--- .gitignore/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Configuration file for storing sensitive information
-
-    // Database connection details
-    define('DB_SERVERNAME', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'electronix'); 
-?>
